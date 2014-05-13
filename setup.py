54e0492def24a6cadab78004dcebb2342e68367a
Arthur Noel <arthur@0compute.net>
2014-05-13 22:18:37 +0100

    a new year

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright 2012-2013 Arthur Noel
+# Copyright 2012-2014 Arthur Noel
 #
 # This file is part of Stacked.
 #
--- stacked/stacked.py
+++ stacked/stacked.py
@@ -1,4 +1,4 @@
-# Copyright 2012-2013 Arthur Noel
+# Copyright 2012-2014 Arthur Noel
 #
 # This file is part of Stacked.
 #
